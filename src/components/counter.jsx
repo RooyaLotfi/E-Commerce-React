e10a7bdac45b5fbfcfd79391fa2b4f8ce2778def
Roya Lotfi <lotfir@myumanitoba.ca>
2020-10-29 15:06:29 -0500

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,17 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
-    render() {
+    renderTages() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)}</ul>;
+    }
 
+    render() {
         return (
             <React.Fragment>
                 <img src={this.getImage()} alt="" />
-                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+                <div>{this.renderTages()}</div>
+                {this.state.tags.length === 0 && 'Please create a new tag'}
             </React.Fragment>
         );
     };
@@ -22,22 +25,11 @@ class Counter extends Component {
         fontSize: 50,
     };
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count == 0 ? "warning" : "primary";
-        return classes;
-    }
-
     getImage() {
         const url = 'https://picsum.photos/500';
         return url;
     }
 
-    formatCount() {
-        const { count } = this.state;
-        const x = "Zero"
-        return count == 0 ? x : count;
-    }
 }
 
 export default Counter;
