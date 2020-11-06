fbc09b2982b4ff43bbafa84087305b030a68ede9
Roya Lotfi <lotfir@myumanitoba.ca>
2020-11-06 13:48:42 -0600

    added class counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,3 +1,11 @@
 //type imrc which is short for import react component
 import React, { Component } from 'react'
 // importing React component is important 
+
+class Counter extends Component {
+    render() {
+        return (<h1>Hello world</h1>);
+    }
+}
+
+export default Counter;
